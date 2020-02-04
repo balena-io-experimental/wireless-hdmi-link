1d6febbf25f65248a5d4a2860e859797eae47f0c
Chris Crocker-White <chriscw@balena.io>
2020-02-04 19:27:23 +0000

    Changed to gstreamer
    Change-type: patch
    Signed-off-by: Chris Crocker-White <chriscw@balena.io>

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ if [ -c "$VIDEODEV" ]; then
     echo "I see a video input, looks like I am the master"
     
     # Load the HDMI capture board with the valid modes (via EDID)
-    v4l2-ctl --set-edid=file=1080P50EDID.txt --fix-edid-checksums
+    v4l2-ctl --set-edid=file=1080P60EDID.txt --fix-edid-checksums
         
     # Find the IPs of the other devices in the app, so we know where to send the output
     IPS=$(curl -sX GET "https://api.balena-cloud.com/v5/application($BALENA_APP_ID)?\$expand=owns__device" \
@@ -20,18 +20,16 @@ if [ -c "$VIDEODEV" ]; then
     # Set the device timings to those detected on the link
     v4l2-ctl --set-dv-bt-timings query
     
+    
     exec gst-launch-1.0 -v \
       v4l2src \
       ! "video/x-raw,framerate=60/1,format=UYVY" \
       ! videorate max-rate=25 \
       ! v4l2h264enc \
-      ! h264parse \
-      ! mpegtsmux \
-      ! rtpmp2tpay \
-      ! multiudpsink clients=10.19.0.229:8888,$IPS
+      ! rtph264pay \
+      ! multiudpsink clients=$IPS
 
 else 
     echo "I don't see any input, I must be an output device"
-    while : ; do echo "${MESSAGE=Idling...}"; sleep ${INTERVAL=600}; done
-    # exec omxplayer -g --live udp://0.0.0.0:8888?listen --aspect-mode stretch
+    exec gst-launch-1.0 -v udpsrc port=8888 caps = "application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96" ! rtph264depay ! decodebin ! videoconvert ! autovideosink
 fi
